4c05ca8b25540438b918a9d833975fa497bddb53
chris <he1237596@163.com>
2024-05-11 15:39:15 +0800

    feat: 疯狂轮播

--- my-pilet/src/Cornerstone/viewport.jsx
+++ my-pilet/src/Cornerstone/viewport.jsx
@@ -9,16 +9,14 @@ import Hammer from "hammerjs";
 import queryString from 'query-string';
 import request from '../request';
 
-import { host, prefix} from "../config";
+const host = 'http://192.168.18.196:16600';
+const prefix = '/api/main'
 
 cornerstoneTools.external.cornerstone = cornerstone;
 cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
 cornerstoneWebImageLoader.external.cornerstone = cornerstone;
 cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
 cornerstoneWADOImageLoader.external.dicomParser = dicomParser
-
-console.log(cornerstoneTools.version)
-
 cornerstoneWADOImageLoader.configure({
   beforeSend: function(xhr) {
     xhr.setRequestHeader("pv-token", localStorage.getItem('token'))
@@ -29,11 +27,11 @@ cornerstoneTools.init({
   // 当元素被启用时，是否监听鼠标事件
   mouseEnabled: true,
   // 当元素被启用时，是否监听触摸事件
-  touchEnabled: false,
+  touchEnabled: true,
   // 全局工具同步
   globalToolSyncEnabled: true,
   // 显示svg光标
-  showSVGCursors: false,
+  showSVGCursors: true,
   // 自动调整视口大小
   autoResizeViewports: true,
   // 虚线样式
@@ -42,11 +40,11 @@ cornerstoneTools.init({
 
 function CornerstoneElement(props) {
   const [viewport, setViewport] = useState(cornerstone.getDefaultViewport(null, undefined));
-  const [stack, setStack] = useState(props.stack);
   const elementRef = useRef(null);
 
   useLayoutEffect(() => {
     const element = elementRef.current;
+
     const onImageRendered = () => {
       const newViewport = cornerstone.getViewport(elementRef.current);
       setViewport(newViewport);
@@ -111,20 +109,29 @@ function CornerstoneElement(props) {
         await fetchToken();
         const response = await request(`/patient/37317ff0-47c2-4372-b4d4-43b35018df3c/all`);
         console.log(response);
-
         const seriesUrls = getDicomUlr(response.data);
         console.log(seriesUrls);
-        // renderNextImage(seriesUrls[0]);
-        setStack({
-          currentImageIdIndex: 0,
-          imageIds: seriesUrls[0],
-        })
+        renderNextImage(seriesUrls[0]);
       } catch (error) {
         console.error('Error fetching patient data:', error);
       }
     };
 
-    // fetchToken()
+
+    const renderNextImage = (imageIds) => {
+      let currentIndex = 0;
+      console.log(imageIds[currentIndex])
+      const renderNextFrame = () => {
+        cornerstone.loadImage(imageIds[currentIndex]).then(image => {
+          cornerstone.displayImage(element, image);
+        });
+        currentIndex = (currentIndex + 1) % imageIds.length;
+        requestAnimationFrame(renderNextFrame);
+      };
+      renderNextFrame();
+    };
+
+    cornerstone.enable(element);
     fetchData();
     return () => {
       cornerstone.disable(element);
@@ -133,104 +140,9 @@ function CornerstoneElement(props) {
   }, []);
 
   useLayoutEffect(() => {
-    const element = elementRef.current;
-/*     const renderNextImage = (imageIds, currentIndex = 0) => {
-      // let currentIndex = 0;
-      // console.log(imageIds[currentIndex])
-      const renderNextFrame = () => {
-        console.log(11111, imageIds[currentIndex])
-        cornerstone.loadImage(imageIds[currentIndex]).then(image => {
-          cornerstone.displayImage(element, image);
-          cornerstoneTools.addStackStateManager(element, ["stack"]);
-          console.log(stack)
-          cornerstoneTools.addToolState(element, "stack", stack);
-          cornerstoneTools.addTool(StackScrollTool, {
-            configuration: {
-              loop: true,
-              allowSkipping: true
-            }
-          });
-          cornerstoneTools.addTool(StackScrollMouseWheelTool, {
-            configuration: {
-              loop: true,
-              allowSkipping: true,
-              invert: false
-            }
-          });
-          cornerstoneTools.setToolActive("StackScroll", { mouseButtonMask: 1 });
-          cornerstoneTools.setToolActive("StackScrollMouseWheel", {
-            mouseButtonMask: 4
-          });
-        });
-        // currentIndex = (currentIndex + 1) % imageIds.length;
-        // requestAnimationFrame(renderNextFrame);
-      };
-      renderNextFrame();
-    }; */
-    console.log(stack && stack.imageIds && stack.imageIds.length > 0)
-    if (stack && stack.imageIds && stack.imageIds.length > 0) {
-      console.log(stack.imageIds[stack.currentImageIdIndex], 1111111)
-      // renderNextImage(stack.imageIds, stack.currentImageIdIndex);
-      cornerstone.enable(element);
-      const StackScrollTool = cornerstoneTools.StackScrollTool;
-      const StackScrollMouseWheelTool = cornerstoneTools.StackScrollMouseWheelTool;
-      const renderNextFrame = (imageIds, currentIndex) => {
-        console.log(11111, imageIds[currentIndex])
-        cornerstone.loadImage(imageIds[currentIndex]).then(image => {
-          cornerstone.displayImage(element, image);
-          cornerstoneTools.addStackStateManager(element, ["stack"]);
-          console.log(stack)
-          cornerstoneTools.addToolState(element, "stack", stack);
-          cornerstoneTools.addTool(StackScrollTool, {
-            configuration: {
-              loop: true,
-              allowSkipping: true
-            }
-          });
-          cornerstoneTools.addTool(StackScrollMouseWheelTool, {
-            configuration: {
-              loop: true,
-              allowSkipping: true,
-              invert: false
-            }
-          });
-          // cornerstoneTools.setToolActive("StackScroll", { mouseButtonMask: 1 });
-          // cornerstoneTools.setToolActive("StackScrollMouseWheel", {
-          //   mouseButtonMask: 4
-          // });
-          const toolName = "Wwwc";
-          cornerstoneTools.addTool(cornerstoneTools.WwwcTool);
-          /**
-           * options 配置如下：
-           * mouseButtonMask 指定鼠标按键：1-鼠标左键、2-鼠标右键、4-鼠标滚轮
-           */
-          const options = {
-            mouseButtonMask: 1
-          };
 
-          // 给指定启用元素激活指定工具
-          cornerstoneTools.setToolActiveForElement(element, toolName, options);
-          // 或者，直接激活全部启用元素的指定工具
-          cornerstoneTools.setToolActive(toolName, options);
 
-          console.log(cornerstoneTools.getToolForElement(element, toolName), 2222)
-          console.log(cornerstoneTools.getToolState(element, "stack"), 2222)
-          console.log(cornerstoneTools.getToolForElement(element, "StackScroll"))
-          // cornerstoneTools.removeToolForElement(element, "StackScroll");
-          // cornerstoneTools.removeToolForElement(element, "StackScrollMouseWheel");
-
-          // 或者，直接清除全部启用元素的指定工具
-          // cornerstoneTools.removeTool('StackScroll');
-          // cornerstoneTools.removeTool('StackScrollMouseWheel');
-        });
-
-        // currentIndex = (currentIndex + 1) % imageIds.length;
-        // requestAnimationFrame(renderNextFrame);
-      };
-      renderNextFrame(stack.imageIds, stack.currentImageIdIndex)
-    }
-
-  }, [stack])
+  }, [])
 
   return (
     <div>
